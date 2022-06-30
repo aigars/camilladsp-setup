816b3fe885d8b275ff121f67e50c14746161a848
dsp-user <aigars.susters@gmail.com>
2022-06-30 23:00:09 +0300

    Fix amixer settings service

--- scripts/amixer-set-autosync.sh
+++ scripts/amixer-set-autosync.sh
@@ -2,10 +2,5 @@
 
 sleep 5
 
-amixer -c0 cset iface=MIXER,name='Preferred Sync Reference' 0
-amixer -c1 cset iface=MIXER,name='Preferred Sync Reference' 9
-amixer -c2 cset iface=MIXER,name='Preferred Sync Reference' 9
-
-amixer -c0 cset iface=MIXER,name='System Clock Mode' 0
 amixer -c1 cset iface=MIXER,name='System Clock Mode' 1
 amixer -c2 cset iface=MIXER,name='System Clock Mode' 1
